b4bd09b2bc802962635a962d2d639680f3c0c4c0
Артём <111016733+ArkXo@users.noreply.github.com>
2024-06-14 15:57:29 +0000

    вв

---
+++ lab_1_task_6.py
@@ -0,0 +1,7 @@
+a = []
+a.append(input("Введите свой возраст: "))
+a.append(input("Введите свой пол: "))
+a.append(input("Введите своё имя: "))
+a.append(input("Введите свой город: "))
+a.append(input("Введите в каком классе вы учитесь: "))
+print(a)
