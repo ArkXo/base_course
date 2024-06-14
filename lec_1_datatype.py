dce1d93400d333c941392b80f7225cb83a156e9f
Артём <111016733+ArkXo@users.noreply.github.com>
2024-06-14 13:34:34 +0000

    ttryt

---
+++ lec_1_datatype.py
@@ -0,0 +1,12 @@
+type(3)
+print(type(3))
+print(type(3.4))
+print(type("Hi"))
+a = "Деконфайнмент кварков"
+print(type(a))
+a = ["Hi", 1, "Деконфайнмент кварков", 6, 8]
+print(type(a))
+print(type(True))
+print(type(False))
+print(type(None))
+print(print("Ба"))
