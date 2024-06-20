481fdb8677156965e7e485b917a292d284ddff69
Артём <111016733+ArkXo@users.noreply.github.com>
2024-06-20 14:02:43 +0000

    et

---
+++ lec_3_from_as.py
@@ -0,0 +1,4 @@
+from lec_3_my_module import earth_mass as em
+from lec_3_my_module import gravity_constant as G
+from lec_3_my_module import sigma_steff_bolc as sigma
+
---
+++ lec_3_import_as.py
@@ -0,0 +1,8 @@
+import lec_3_my_module as lm
+
+print(lm.a)
+
+b = lm.b * 3
+print(b)
+
+print(lm.c[2] + b + lm.c[0])
