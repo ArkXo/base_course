c86bd6c51b2a5743022ae4c4566db977d6e156af
Артём <111016733+ArkXo@users.noreply.github.com>
2024-06-20 16:10:15 +0000

    jj

--- lab_3_task2.py
+++ lab_3_task2.py
@@ -1,9 +1,13 @@
-import lab_3_task1 as cons
+import lab_3_task1 as const
 import numpy as np
 
 h = 100
-a = cons.pi / 3
-b = cons.pi / 6
-v = np.sqrt((cons.g * h * np.tan(b)**2)/(2 * np.cos(a)**2 * (1 - np.tan(b) * np.tan(a))))
+a = const.pi / 3
+b = const.pi / 6
+v = np.sqrt((const.g * h * np.tan(b)**2)/(2 * np.cos(a)**2 * (1 - np.tan(b) * np.tan(a))))
 print(v)
 
+T = 200
+E = 300
+N = (2 * const.ht * (const.e**( (-E) / (const.k * T))) * (E ** ( T/2 ))) / (np.sqrt(const.pi) * (const.k * T)**(3/2))
+print(N)
