64ff85a81fddacb395a9b27e27954c3313e75741
Артём <111016733+ArkXo@users.noreply.github.com>
2024-06-21 12:03:23 +0000

    geer

--- lab_3_task3.py
+++ lab_3_task3.py
@@ -5,10 +5,12 @@ v0x = int(input("Введите скорость тела: "))
 x0 = int(input("Введите начальные координаты по x: "))
 y0 = int(input("Введите начальные координаты по y: "))
 
-a = ["t", "x", 'y']
-mas = np.array([a])
-print(mas)
+mas = [["t", "x", 'y']]
+
 for t in range(0, 6):
     x = x0 + v0x * t
     y = y0 + v0x * t - g * t**2 / 2
+    mas.append([t, x, y])
 
+mas = np.array(mas)
+print(mas)
