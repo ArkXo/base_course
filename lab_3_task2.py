5c388eedc279764ad1f36e314ee8b41b5c15c625
Артём <111016733+ArkXo@users.noreply.github.com>
2024-06-25 14:09:14 +0000

    епк

--- lab_3_task2.py
+++ lab_3_task2.py
@@ -7,7 +7,7 @@ b = 35 / 180 * np.pi
 v = np.sqrt((const.g * h * np.tan(b)**2)/(2 * np.cos(a)**2 * (1 - np.tan(b) * np.tan(a))))
 print(v)
 
-T = 200
+T = 200 
 E = 300
 from lab_3_task1 import k
 from lab_3_task1 import ht
