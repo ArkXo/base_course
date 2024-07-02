9a4c68e3190d450c9970fdedc314b7b880894764
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-02 13:24:36 +0000

    2w

--- lab_3_extratask2.py
+++ lab_3_extratask2.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-mas = np.zeros()
-# while len(mas) <= 10:
-#     a = input()
-#     if a == " ":
-#         break
-#     else:
-#         np.append(mas, [int(a)])
-# a = input()
-np.append(mas, [int(input())])
-print(mas)
+array = np.array([])
+while len(array) <= 10:
+    a = input()
+    if a == "":
+        break
+    a = int(a)
+    array = np.append(array, [a])
+print(array)
