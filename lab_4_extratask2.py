ed1288961447b8a17e672a38daf7cc792a0630c0
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-09 15:28:37 +0000

    тмт

--- lab_4_extratask2.py
+++ lab_4_extratask2.py
@@ -1,9 +1,14 @@
 def fib(n):
     fib_1 = 0
     fib_2 = 1
-    for i in range(n-1):
-        fib_n = fib_1 + fib_2
-        fib_1, fib_2 = fib_2, fib_n
+    if n > 2:
+        for i in range(n-2):
+            fib_n = fib_1 + fib_2
+            fib_1, fib_2 = fib_2, fib_n
+    elif n == 1:
+        fib_n = fib_1
+    elif n == 2:
+        fib_n = fib_2
     return fib_n
 
 print(fib(int(input("Введите значение n: "))))
