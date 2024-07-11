4a207322863d78b6b20561fa9a0d1388da036d16
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-11 14:22:44 +0000

    fg

--- lab_6_task2.py
+++ lab_6_task2.py
@@ -2,19 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def hyper(minimum, maximum, N):
-    x = np.linspace(0, maximum, N)
-    y = 20/x
-    plt.plot(x, y, label="my hyperbola")
+    N = int(N/2)
 
-    x = np.linspace(minimum, 0, N)
+    x = np.linspace(0, maximum, N+1)
+    x = np.delete(x, 0)
     y = 20/x
-    plt.plot(x, y, label="my hyperbola")
+    plt.plot(x, y, color='b', label="my hyperbola")
+
+    x = np.linspace(minimum, 0, N+1)
+    x = np.delete(x, -1)
+    y = 20/x
+    plt.plot(x, y, color='b')
 
     plt.xlabel("Coord - x")
     plt.ylabel("Coord - y")
     plt.title("Parabola")
     plt.legend()
+    plt.grid()
     
-    plt.savefig('fig_3.png')
+    plt.savefig('fig_task2.png')
 
 hyper(int(input("Введите значение минимума х: ")), int(input("Введите значение максимума х: ")), int(input("Введите значение N: ")))
