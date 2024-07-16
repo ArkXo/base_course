095408ac40544f7c2b3f8aa049c81abf6fa29d09
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-16 17:01:17 +0000

    fndf

--- lab_task3_butterfly.py
+++ lab_task3_butterfly.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def bttrfl(t):
-    x = np.sin(t)
-    y = np.cos(t)
+    x = np.sin(t) * ((np.e**np.cos(t)) - (2*np.cos(4*t)) + (np.sin(t/12)**5))
+    y = np.cos(t) * ((np.e**np.cos(t)) - (2*np.cos(4*t)) + (np.sin(t/12)**5))
     return x, y
 
 def animate(t):
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     ax.set_ylim(-5, 5)
     
 
-    ani = FuncAnimation(fig, animate, frames=np.linspace(0, 2*np.pi, 100), interval=100)
+    ani = FuncAnimation(fig, animate, frames=np.arange(0, 12.1*np.pi, 0.1), interval=100)
 
-    ani.save("animation_butterfly.gif")
+    ani.save('animation_butterfly.gif')
     
