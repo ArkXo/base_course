35fa13b583e5ed2e82fccb693d1f203d8dde6516
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-16 12:32:38 +0000

    feb

--- lab_6_extratask4.py
+++ lab_6_extratask4.py
@@ -1,12 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# def steps(n):
-#     x = np.arrange(0, n+1, 1)
-#     y = np.array([1])
-#     for i in range(0, n+1):
+def steps(n):
+    x = np.arange(0, 3+2, 1)
+    new_x = np.array([])
+    y = np.array([])
+    for i in range(0, 4):
+        new_x = np.append(new_x, [x[i], x[i]])
+        y = np.append(y, [x[i], x[i]])
+    new_x = np.delete(new_x, 0)
+    y = np.delete(y, -1)
+    print(new_x)
 
-x = np.arange(0, 3, 1)
-for i in range(0, 3):
-    new_x = np.insert(x, i, x[i])
-print(new_x)
+    plt.plot(new_x, y, label="Ступеньки")
+    plt.xlabel("Coord - x")
+    plt.ylabel("Coord - y")
+    plt.title("Refund")
+    plt.legend()
+    plt.grid()
+    plt.axis('equal')
+    
+    plt.savefig('fig_extratask4.png')
+
+steps(int(input("Введите значение количества ступенек: ")))
+
+# x = np.arange(0, 3+2, 1)
+# print(x)
