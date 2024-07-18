f832424a50486e58479eef0dbe8381c333ce87c3
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-18 12:58:12 +0000

    y6

--- lab_task3_butterfly.py
+++ lab_task3_butterfly.py
@@ -2,24 +2,27 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 
-def bttrfl(t):
+def bttrfl(i):
+    t = np.arange(i- (2*np.pi), i, 0.01)
     x = np.sin(t) * ((np.e**np.cos(t)) - (2*np.cos(4*t)) + (np.sin(t/12)**5))
     y = np.cos(t) * ((np.e**np.cos(t)) - (2*np.cos(4*t)) + (np.sin(t/12)**5))
     return x, y
 
-def animate(t):
-    butterfly.set_data(bttrfl(t=t))
 
-if __name__ == "__main__":
+def animate(i):
+    butterfly.set_data(bttrfl(i=i))
 
-    fig, ax = plt.subplots()
-    butterfly, = plt.plot([], [], '-', color='r', label='Butterfly')
 
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
+
+fig, ax = plt.subplots()
+butterfly, = plt.plot([], [], '-', color='r', label='Butterfly')
+
+edge = 5
+plt.axis('equal')
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
     
 
-    ani = FuncAnimation(fig, animate, frames=np.arange(0, 12.1*np.pi, 0.1), interval=100)
+ani = FuncAnimation(fig, animate, frames=np.arange(0, 12.1*np.pi, np.pi/2), interval=200)
 
-    ani.save('animation_butterfly.gif')
-    
+ani.save("animation_butterfly.gif")
