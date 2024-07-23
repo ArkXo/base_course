a89a8d25a391452801ab05a1453d7723bf0e4341
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-23 13:30:33 +0000

    tliu

--- lab_7_task4.py
+++ lab_7_task4.py
@@ -2,7 +2,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 
-def bttrfl(n):
+def frctl(n):
     x = [0]
     y = [0]
     C = 0.3
@@ -10,18 +10,18 @@ def bttrfl(n):
     for i in range(0, n-1):
         x.append( x[-1]**2 - y[-1]**2 + C )
         y.append( 2 * x[-1] * y[-1] + D )
-    ax.set_xlim(-x[-1]*2, x[-1]*2)
-    ax.set_ylim(-y[-1]*2, y[-1]*2)
+        ax.set_xlim((x[-1])-0.01/(i+10000*(i+1)), x[-1]+0.01/(i+10000*(i+1)))
+        ax.set_ylim((y[-1])-0.01/(i+10000*(i+1)), y[-1]+0.01/(i+10000*(i+1)))
     return x, y
 
 
 def animate(w):
-    butterfly.set_data(bttrfl(n=w))
+    fractal.set_data(frctl(n=w))
 
 
 
 fig, ax = plt.subplots()
-butterfly, = plt.plot([], [], '-', color='r', label='Butterfly', lw = 1)
+fractal, = plt.plot([], [], '-', color='r', label='Butterfly', lw = 1)
 
 
 ani = FuncAnimation(fig, animate, frames=np.arange(0, int(input()), 1), interval=100)
