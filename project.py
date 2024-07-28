5f4a8b6bf7d383568994c5732c55bb3f2cee3e8d
Артём <111016733+ArkXo@users.noreply.github.com>
2024-07-28 22:39:56 +0000

    оьен

--- project.py
+++ project.py
@@ -9,14 +9,11 @@ angle_vel_earth = 0.9856 # градус в день
 # Угловая скорость Юпитера:
 angle_vel_jupiter = 0.083 # градус в день
 
-# Данные для гловой скорости космического корабля:
-G = 6.67 * 10**(-11)
-mass_son = 1.989 * 10**30
+# Данные для угловой скорости космического корабля:
 a = 3.1
+b = 2.28
+T = 1994
 e = 0.677
-p = 1.677
-
-
 
 # Движение Земли:
 
@@ -48,7 +45,7 @@ def move_earth_line(time):
 
 def move_jupiter(time):
 
-    alpha = angle_vel_jupiter * np.pi / 180 * time
+    alpha = angle_vel_jupiter * np.pi / 180 * time + np.pi - angle_vel_jupiter * np.pi / 180 * T /2
 
     x0 = 5.2 * np.cos(alpha)
     y0 = 5.2 * np.sin(alpha)
@@ -62,7 +59,7 @@ def move_jupiter(time):
 
 def move_jupiter_line(time):
 
-    alpha = angle_vel_jupiter * np.pi / 180 * time
+    alpha = angle_vel_jupiter * np.pi / 180 * time + np.pi - angle_vel_jupiter * np.pi / 180 * T /2
     alpha = np.arange(0, alpha, 0.01)
 
     x = 5.2 * np.cos(alpha)
@@ -74,28 +71,36 @@ def move_jupiter_line(time):
 
 def move_spaceship(time):
 
-    r = p / (1-e*np.cos(alpha))
-    angle_vel = np.sqrt(mass_son*G) * np.sqrt( (2*a-r) / ((r**3) * a))
-    alpha = angle_vel * np.pi / 180 * time
-    
+    M = 2 * np.pi * time / T
 
-    x0 = 5.2 * np.cos(alpha)
-    y0 = 5.2 * np.sin(alpha)
+    for j in np.arange(0, 3*np.pi, 0.0001):
+        fxy = j - e * np.sin(j) - M
+        if fxy >=0.001:
+            E = j
+            break
+
+    x0 = a * np.cos(E) - (a-1)
+    y0 = b * np.sin(E)
 
     phi = np.arange(0, 2*np.pi, 0.5)
 
     x = x0 + 0.01*np.cos(phi)
     y = y0 + 0.01*np.sin(phi)
-
     return x, y
 
 def move_spaceship_line(time):
 
-    alpha = angle_vel_jupiter * np.pi / 180 * time
-    alpha = np.arange(0, alpha, 0.01)
+    M = 2 * np.pi * time / T
 
-    x = 5.2 * np.cos(alpha)
-    y = 5.2 * np.sin(alpha)
+    for j in np.arange(0, 3*np.pi, 0.0001):
+        fxy = j - e * np.sin(j) - M
+        if fxy >=0.01:
+            E = j
+            break
+
+    E = np.arange(0,E,0.01)
+    x = a * np.cos(E) - (a-1)
+    y = b * np.sin(E)
 
     return x, y
 
@@ -131,10 +136,10 @@ spaceship_line, = plt.plot( [], [], '-', color='g')
 spaceship, = plt.plot( [], [], 'o', color='r', lw="0.5")
 
 edge = 6
-plt.axis('equal')
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
+plt.axis('scaled')
 
-ani = FuncAnimation(fig, animate, frames=np.arange(0, 365, 5), interval=0.01)
+ani = FuncAnimation(fig, animate, frames=np.arange(0, 1000, 3), interval=0.01)
 
 ani.save("project.gif")
