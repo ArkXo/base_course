f2e5ec5491d5efd22cac940c15970aab2b0c1385
Артём <111016733+ArkXo@users.noreply.github.com>
2024-10-26 11:21:02 +0000

    gsbgb

--- lab_1_task3.py
+++ lab_1_task3.py
@@ -4,8 +4,24 @@ import matplotlib.pyplot as plt
 
 m = 3
 a_0 = 10
+v_0 = 0
 gamma = 0.2
-t = np.arrange(0, 10**3, 1)
+t = np.linspace(0, 7, 10000)
 
-def boost_function():
-    a = (np.sqrt(m**2 + 12*gamma*a_0))
+def boost_function(v, t):
+    dvdt = a_0 - (gamma/m) * v**2
+    return dvdt
+
+v_t = odeint(boost_function, v_0, t)
+print(v_t)
+
+
+plt.plot(t, v_t[:,0], label="скорость")
+plt.xlabel("Время, секунды")
+plt.ylabel("Функция скорости")
+plt.title("Закон изменения скорости")
+plt.legend()
+
+plt.savefig("lab_task3.png")
+
+
