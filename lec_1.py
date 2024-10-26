d59beaa3036dfe3c4c0b23cdc6e13ff6f6aa6552
Артём <111016733+ArkXo@users.noreply.github.com>
2024-10-26 13:45:04 +0000

    hgcx

---
+++ lec_1.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# Определяем переменную величину
+t = np.arange(0, 10, 0.01)
+
+# Определяем функцию для системы дифю уравнений
+def diff_func(z, t): # z - изменяемая величина для системы
+    theta, omega = z # Укаханные изменения функций, через z
+
+    # Первое уравнение системы
+    dtheta_dt = omega
+    # Второе уравнение системы
+    domega_dt = - k * omega - c * np.sin(theta)
+
+    return dtheta_dt, domega_dt
+
+# Определяем начальные значения и параметры,
+# входящие в систему диф. уравнений
+theta0 = np.pi - 0.1
+omega0 = 0
+
+z0 = theta0, omega0
+
+k = 0.25
+c = 5.0
+
+sol = odeint(diff_func, z0, t)
+
+plt.plot(t, sol[:, 0], "b", label="theta(t)")
+
+
+plt.legend
+plt.savefig("fig_1.png")
+
---
+++ lec_2.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# Определяем переменную величину
+x = np.arange(1, 3, 0.01)
+
+# Определяем функцию для системы дифю уравнений
+def diff_func(z, x): # z - изменяемая величина для системы
+    y, omega = z # Укаханные изменения функций, через z
+
+    # Первое уравнение системы
+    dy_dx = omega
+    # Второе уравнение системы
+    domega_dx = np.sin(y)*omega - 3*x*y - 5
+
+    return dy_dx, domega_dx
+
+# Определяем начальные значения и параметры,
+# входящие в систему диф. уравнений
+y0 = 0.01
+omega0 = 0.05
+
+z0 = y0, omega0
+
+sol = odeint(diff_func, z0, x)
+
+plt.plot(x, sol[:, 0], "b", label="y(t)")
+plt.plot(x, sol[:, 1], "g", label="omaga(t)")
+
+
+plt.legend
+plt.savefig("fig_2.png")
+
