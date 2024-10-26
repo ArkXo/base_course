adc3f02c9ae0cda838b7dde2e24809bccff088f9
Артём <111016733+ArkXo@users.noreply.github.com>
2024-10-26 11:55:58 +0000

    c

--- lab_1_task2.py
+++ lab_1_task2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 n_0 = 1000
 k = 0.08
-t_0 = 4*365
+t_0 = 1.5*365
 t = np.arange(0, t_0, 1)
 
 print("Закон изменения инвестиций: v = 0.08 * n,\nгде v - скорость, n - инвестируемые в данный момент времени средства, k - коэфицент пропорциональности.")
@@ -18,9 +18,8 @@ n_t = odeint(investment_function, n_0, t)
 
 print(n_t)
 
-
 plt.plot(t, n_t[:,0], label="Инвестиции")
-plt.xlabel("Время уменьшения, секунды")
+plt.xlabel("Время уменьшения, дни")
 plt.ylabel("Функция уменьшения")
 plt.title("Закон уменьшения")
 plt.legend()
