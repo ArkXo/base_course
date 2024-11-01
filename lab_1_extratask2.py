c75d2246cf553124a6a34132bcb80b18c3be3358
Артём <111016733+ArkXo@users.noreply.github.com>
2024-11-01 11:39:50 +0000

    BB

--- lab_1_extratask2.py
+++ lab_1_extratask2.py
@@ -4,14 +4,12 @@ import matplotlib.pyplot as plt
 
 S_0 = 0.16
 E_0 = 1360
-k = 0.000039
+k = 0.00005
 S =[]
 
-t = np.arange(0, 24, 1)
+t = np.arange(0, 24, 0.1)
 
 for i in np.arange(0, 24, 0.1):
-    print()
-    print(i)
     if i >= 6 and i <= 18:
         alpha = abs(12-i)*15 * np.pi/180
         dSdt = k * E_0*np.cos(alpha)*np.sqrt((S_0**3)/np.pi)
@@ -19,22 +17,24 @@ for i in np.arange(0, 24, 0.1):
         dSdt = 0
     S_0 += dSdt
     S.append(S_0)
-    print(S_0)
+    if i == 12:
+        k = 0.25/(S_0)*k
+        print(k)
 
-print(S)
+S_0 = 0.16
 
-# def func_victoria_rega(S, t):
-#     print(t)
-#     if t >= 6 and t <= 18:
-#         alpha = abs(12-t)*15 * np.pi/180
-#         dsdt = k*E_0*np.cos(alpha)*np.sqrt((S**3)/np.pi)
-#     else:
-#         dsdt = 0
-#     return dsdt
-# 
-# S_t = odeint(func_victoria_rega, S_0, t)
-# 
-plt.plot(t, S, label="скорость")
+def func_victoria_rega(S, t):
+    print(t)
+    if t >= 6 and t <= 18:
+        alpha = abs(12-t)*15 * np.pi/180
+        dsdt = k * E_0*np.cos(alpha)*np.sqrt((S**3)/np.pi)
+    else:
+        dsdt = 0
+    return dsdt
+
+S_t = odeint(func_victoria_rega, S_0, t)
+
+plt.plot(t, S_t, label="Площадь")
 plt.xlabel("Время, секунды")
 plt.ylabel("Функция скорости")
 plt.title("Закон изменения скорости")
