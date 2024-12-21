9d16e4c2ba41277da95cc3a6d4df9568f06b4e93
Артём <111016733+ArkXo@users.noreply.github.com>
2024-12-21 12:26:09 +0000

    ор

--- lab_4_extratask1.py
+++ lab_4_extratask1.py
@@ -7,13 +7,17 @@ def func(r, N, v, x_0, y_0):
     X = np.array([])
     Y = np.array([])
     for i in range(0, N+1):
-        x = r * np.cos(360*i/N) + x_0
-        y = r * np.cos(360*i/N) + y_0
+        x = r * np.cos(2*np.pi*i/N) + x_0
+        y = r * np.sin(2*np.pi*i/N) + y_0
         X = np.append(X, [x])
         Y = np.append(Y, [y])
 
     return X, Y
 
-sol = func(5, 100, 2, 3, 4)
-x = sol[0]
-print(x)
+sol = func(5, 10, 2, 3, 4)
+t = sol[0]
+s = sol[1]
+print(t)
+
+plt.plot(t, s, label="Площадь")
+plt.savefig("lab_extratask1.png")
