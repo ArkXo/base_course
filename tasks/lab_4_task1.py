bbf811daf07f6dbd7c67e8c3ece8af0471d24980
Артём <111016733+ArkXo@users.noreply.github.com>
2024-12-21 09:26:22 +0000

    ss

--- tasks/lab_4_task1.py
+++ tasks/lab_4_task1.py
@@ -10,14 +10,15 @@ years = 1
 t = np.linspace(0, years*seconds_in_year, frames)
 
 G = 6.67 * 10**(-11)
-M = 1.98 * 10**(30)
-ae = 149 * 10**9
+M = 1.989 * 10**(30)
+ae = 149.6 * 10**9
 v_earth = 29800
  
 s0_merc = 0.387*ae, 0, 0, v_earth/np.sqrt(0.387)
 s0_ven = 0.723*ae, 0, 0, v_earth/np.sqrt(0.723)
 s0_earth = 1*ae, 0, 0, v_earth
-      [1.52*ae, 0, 0, v_earth/np.sqrt(1.52)])
+s0_mars = 1.52*ae, 0, 0, v_earth/np.sqrt(1.52)
+s0_pha = 2.4*ae, 0, 0, ( (G*M * (1-0.88994)) / (2.4*ae) )**(1/2)
 
 def move_func(s, t):
     x, v_x, y, v_y = s
@@ -28,29 +29,66 @@ def move_func(s, t):
 
     return dx_dt, dvx_dt, dy_dt, dvy_dt
 
-sol = odeint(move_func, s0, t)
+sol_merc = odeint(move_func, s0_merc, t)
+sol_ven = odeint(move_func, s0_ven, t)
+sol_earth = odeint(move_func, s0_earth, t)
+sol_mars = odeint(move_func, s0_mars, t)
+sol_pha = odeint(move_func, s0_pha, t)
 
-x = sol[:, 0] / ae
-y = sol[:, 2] / ae
+x_merc = sol_merc[:, 0] / ae
+y_merc = sol_merc[:, 2] / ae
+
+x_ven = sol_ven[:, 0] / ae
+y_ven = sol_ven[:, 2] / ae
+
+x_earth = sol_earth[:, 0] / ae
+y_earth = sol_earth[:, 2] / ae
+
+x_mars = sol_mars[:, 0] / ae
+y_mars = sol_mars[:, 2] / ae
+
+x_pha = sol_pha[:, 0] / ae
+y_pha = sol_pha[:, 2] / ae
 
 
 def animate(i):
-    ball.set_data([x[i]], [y[i]])
+    ball_merc.set_data([x_merc[i]], [y_merc[i]])
+    ball_merc_line.set_data([x_merc[:i]], [y_merc[:i]])
 
-    ball_line.set_data([x[:i]], [y[:i]])
+    ball_ven.set_data([x_ven[i]], [y_ven[i]])
+    ball_ven_line.set_data([x_ven[:i]], [y_ven[:i]])
+
+    ball_earth.set_data([x_earth[i]], [y_earth[i]])
+    ball_earth_line.set_data([x_earth[:i]], [y_earth[:i]])    
+    
+    ball_mars.set_data([x_mars[i]], [y_mars[i]])
+    ball_mars_line.set_data([x_mars[:i]], [y_mars[:i]])
+
+    ball_pha.set_data([x_pha[i]], [y_pha[i]])
+    ball_pha_line.set_data([x_pha[:i]], [y_pha[:i]])
 
 fig, ax = plt.subplots()
 plt.axis("equal")
-edge = 2
+edge = 2.5
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
 plt.plot([0], [0], 'o', color='y', ms=20)
 
-ball, = plt.plot([], [], 'o', color='b')
-ball_line, = plt.plot([], [], '-', color='b')
+ball_merc_line, = plt.plot([], [], '-', color='0')
+ball_merc, = plt.plot([], [], 'o', color='m')
 
+ball_ven_line, = plt.plot([], [], '-', color='0')
+ball_ven, = plt.plot([], [], 'o', color='g')
 
+ball_earth_line, = plt.plot([], [], '-', color='0')
+ball_earth, = plt.plot([], [], 'o', color='b')
+
+ball_mars_line, = plt.plot([], [], '-', color='0')
+ball_mars, = plt.plot([], [], 'o', color='r')
+
+ball_pha_line, = plt.plot([], [], '-', color='0')
+ball_pha, = plt.plot([], [], 'o', color='tab:gray')
 
 ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 ani.save("gifs/task1.gif")
---
+++ tasks/lab_4_task2.py
@@ -0,0 +1,102 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+ 
+# Определяем переменную величину
+frames = 500
+seconds_in_year = 365 * 24 * 60 * 60
+years = 1
+t = np.linspace(0, years*seconds_in_year, frames)
+
+k = 9 * 10**9
+Q = 15
+ae = 10**3
+v = 29
+ 
+s0_1 = 0.387*ae, 0, 0, v/np.sqrt(0.387), 3, 12
+s0_2 = 0.723*ae, 0, 0, v/np.sqrt(0.723), 4, 23
+s0_3 = 1*ae, 0, 0, v, 6, 2
+s0_4 = 1.52*ae, 0, 0, v/np.sqrt(1.52), -5, 3
+s0_5 = 2*ae, 0, 0, v/np.sqrt(2), -3, 15
+s0_6 = 2.57*ae, 0, 0, v/np.sqrt(2.57), -6, 27
+
+def move_func(s, t):
+    x, v_x, y, v_y, q, m = s
+    dx_dt = v_x
+    dvx_dt = - (k * Q * abs(q) * x / m) / (x**2 + y**2)**1.5
+    dy_dt = v_y
+    dvy_dt = - (k * Q * abs(q) * y / m) / (x**2 + y**2)**1.5
+
+    return dx_dt, dvx_dt, dy_dt, dvy_dt
+
+sol_1 = odeint(move_func, s0_1, t)
+sol_2 = odeint(move_func, s0_2, t)
+sol_3 = odeint(move_func, s0_3, t)
+sol_4 = odeint(move_func, s0_4, t)
+sol_5 = odeint(move_func, s0_5, t)
+sol_6 = odeint(move_func, s0_6, t)
+
+x_1 = sol_1[:, 0] / ae
+y_1 = sol_1[:, 2] / ae
+
+x_2 = sol_2[:, 0] / ae
+y_2 = sol_2[:, 2] / ae
+
+x_3 = sol_3[:, 0] / ae
+y_3 = sol_3[:, 2] / ae
+
+x_4 = sol_4[:, 0] / ae
+y_4 = sol_4[:, 2] / ae
+
+x_5 = sol_5[:, 0] / ae
+y_5 = sol_5[:, 2] / ae
+
+x_6 = sol_6[:, 0] / ae
+y_6 = sol_6[:, 2] / ae
+
+
+def animate(i):
+    ball_1.set_data([x_1[i]], [y_1[i]])
+    ball_1_line.set_data([x_1[:i]], [y_1[:i]])
+
+    ball_2.set_data([x_2[i]], [y_2[i]])
+    ball_2_line.set_data([x_2[:i]], [y_2[:i]])
+
+    ball_3.set_data([x_3[i]], [y_3[i]])
+    ball_3_line.set_data([x_3[:i]], [y_3[:i]])    
+    
+    ball_4.set_data([x_4[i]], [y_4[i]])
+    ball_4_line.set_data([x_4[:i]], [y_4[:i]])
+
+        ball_4.set_data([x_4[i]], [y_4[i]])
+    ball_4_line.set_data([x_4[:i]], [y_4[:i]])
+
+fig, ax = plt.subplots()
+plt.axis("equal")
+edge = 2.5
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
+
+plt.plot([0], [0], 'o', color='y', ms=20)
+
+ball_1_line, = plt.plot([], [], '-', color='0')
+ball_1, = plt.plot([], [], 'o', color='m')
+
+ball_2_line, = plt.plot([], [], '-', color='0')
+ball_2, = plt.plot([], [], 'o', color='g')
+
+ball_3_line, = plt.plot([], [], '-', color='0')
+ball_3, = plt.plot([], [], 'o', color='b')
+
+ball_4_line, = plt.plot([], [], '-', color='0')
+ball_4, = plt.plot([], [], 'o', color='r')
+
+ball_5_line, = plt.plot([], [], '-', color='0')
+ball_5, = plt.plot([], [], 'o', color='r')
+
+ball_6_line, = plt.plot([], [], '-', color='0')
+ball_6, = plt.plot([], [], 'o', color='r')
+
+ani = FuncAnimation(fig, animate, frames=frames, interval=30)
+ani.save("gifs/task1.gif")
