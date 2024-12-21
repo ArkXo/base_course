a863e035ccfa84333b300f08fd0b6384e7c5bcf0
Артём <111016733+ArkXo@users.noreply.github.com>
2024-12-21 09:59:04 +0000

    bbb

--- tasks/lab_4_task2.py
+++ tasks/lab_4_task2.py
@@ -11,24 +11,26 @@ t = np.linspace(0, years*seconds_in_year, frames)
 
 k = 9 * 10**9
 Q = 15
-ae = 10**3
-v = 29
+ae = 10**9
+v = 29000
  
-s0_1 = 0.387*ae, 0, 0, v/np.sqrt(0.387), 3, 12
-s0_2 = 0.723*ae, 0, 0, v/np.sqrt(0.723), 4, 23
-s0_3 = 1*ae, 0, 0, v, 6, 2
-s0_4 = 1.52*ae, 0, 0, v/np.sqrt(1.52), -5, 3
-s0_5 = 2*ae, 0, 0, v/np.sqrt(2), -3, 15
-s0_6 = 2.57*ae, 0, 0, v/np.sqrt(2.57), -6, 27
+s0_1 = 0.387*ae, -v/np.sqrt(0.387), 0, 0, 3, 1
+s0_2 = 0.723*ae, -v/np.sqrt(0.723), 0, 0, 4, 2
+s0_3 = 1*ae, -v, 0, 0, 6, 2
+s0_4 = 1.52*ae, -v/np.sqrt(1.52), 0, 0, -5, 3
+s0_5 = 2*ae, -v/np.sqrt(2), 0, 0, -3, 1
+s0_6 = 2.57*ae, -v/np.sqrt(2.57), 0, 0, -6, 2
 
 def move_func(s, t):
     x, v_x, y, v_y, q, m = s
     dx_dt = v_x
-    dvx_dt = - (k * Q * abs(q) * x / m) / (x**2 + y**2)**1.5
+    dvx_dt = -(k * Q * q / m) * x / (x**2 + y**2)**1.5
     dy_dt = v_y
-    dvy_dt = - (k * Q * abs(q) * y / m) / (x**2 + y**2)**1.5
+    dvy_dt = -(k * Q * q / m) * y / (x**2 + y**2)**1.5
+    dq_dt = 0
+    dm_dt = 0
 
-    return dx_dt, dvx_dt, dy_dt, dvy_dt
+    return dx_dt, dvx_dt, dy_dt, dvy_dt, dq_dt, dm_dt
 
 sol_1 = odeint(move_func, s0_1, t)
 sol_2 = odeint(move_func, s0_2, t)
@@ -55,7 +57,6 @@ y_5 = sol_5[:, 2] / ae
 x_6 = sol_6[:, 0] / ae
 y_6 = sol_6[:, 2] / ae
 
-
 def animate(i):
     ball_1.set_data([x_1[i]], [y_1[i]])
     ball_1_line.set_data([x_1[:i]], [y_1[:i]])
@@ -69,12 +70,15 @@ def animate(i):
     ball_4.set_data([x_4[i]], [y_4[i]])
     ball_4_line.set_data([x_4[:i]], [y_4[:i]])
 
-        ball_4.set_data([x_4[i]], [y_4[i]])
-    ball_4_line.set_data([x_4[:i]], [y_4[:i]])
+    ball_5.set_data([x_5[i]], [y_5[i]])
+    ball_5_line.set_data([x_5[:i]], [y_5[:i]])
+
+    ball_6.set_data([x_6[i]], [y_6[i]])
+    ball_6_line.set_data([x_6[:i]], [y_6[:i]])
 
 fig, ax = plt.subplots()
 plt.axis("equal")
-edge = 2.5
+edge = 10
 ax.set_xlim(-edge, edge)
 ax.set_ylim(-edge, edge)
 
@@ -99,4 +103,4 @@ ball_6_line, = plt.plot([], [], '-', color='0')
 ball_6, = plt.plot([], [], 'o', color='r')
 
 ani = FuncAnimation(fig, animate, frames=frames, interval=30)
-ani.save("gifs/task1.gif")
+ani.save("gifs/task2.gif")
